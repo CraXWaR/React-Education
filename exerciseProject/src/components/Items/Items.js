57a5c8e0d20e7ff702355f012eded2522eb10736
CraXWaR <craxwar@gmail.com>
2023-07-03 12:27:37 +0300

    Creating header component

---
+++ exerciseProject/src/components/Items/Items.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+function Items(props) {
+    return (
+        <li className="concept">
+          <img src="TODO: IMAGE" alt="TODO: TITLE" />
+          <h2>TODO: TITLE</h2>
+          <p>TODO: DESCRIPTION</p>
+        </li>
+    );
+}
+
+export default Items;
