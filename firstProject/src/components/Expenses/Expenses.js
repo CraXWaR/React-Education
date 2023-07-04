8fc278133b999c81a2fd4a06a34b499959ee481b
CraXWaR <craxwar@gmail.com>
2023-07-04 16:09:36 +0300

    Adding expense filter component

--- firstProject/src/components/Expenses/Expenses.js
+++ firstProject/src/components/Expenses/Expenses.js
@@ -1,10 +1,12 @@
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
     <Card className="expenses">
+    <ExpensesFilter />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
