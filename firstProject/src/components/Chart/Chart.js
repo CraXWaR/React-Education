a80a8b05752fa4cdbacec1a07dd6422c3844fdfe
CraXWaR <craxwar@gmail.com>
2023-07-05 13:14:54 +0300

    Adding dynamic bar to show the expensed on the year

---
+++ firstProject/src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const dataValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMax = Math.max(...dataValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((data) => (
+        <ChartBar
+          key={data.label}
+          value={data.value}
+          maxValue={totalMax}
+          label={data.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
