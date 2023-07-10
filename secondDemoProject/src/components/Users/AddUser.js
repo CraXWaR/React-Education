5754e8e043777d40b4a04681f92d0cf0bb38d71e
CraXWaR <craxwar@gmail.com>
2023-07-10 16:03:57 +0300

    Creating css modules for both components

--- secondDemoProject/src/components/Users/AddUser.js
+++ secondDemoProject/src/components/Users/AddUser.js
@@ -1,16 +1,22 @@
+import Card from "../UI/Card";
+
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="button">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="button">Add User</button>
+      </form>
+    </Card>
   );
 };
 
