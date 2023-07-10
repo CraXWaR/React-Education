55f857a353a84fe4d930d1d0e666efdb8eb2831d
CraXWaR <craxwar@gmail.com>
2023-07-10 16:18:27 +0300

    Getting entered values from form

--- secondDemoProject/src/components/Users/AddUser.js
+++ secondDemoProject/src/components/Users/AddUser.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
   const addHandler = (event) => {
     event.preventDefault();
+
+    console.log(username, age);
+  };
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <Button type="button">Add User</Button>
+        <input id="age" type="number" onChange={ageHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
