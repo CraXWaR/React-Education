8363784c40ba70cd8c110a270dc4bc3d9eeee191
CraXWaR <craxwar@gmail.com>
2023-07-10 16:53:46 +0300

    Creating error component and using it

--- secondDemoProject/src/components/Users/AddUser.js
+++ secondDemoProject/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 
 import classes from "./AddUser.module.css";
 
@@ -34,20 +35,23 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={usernameHandler}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value={age} onChange={ageHandler} />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="Error" message="smth not ok" />
+      <Card className={classes.input}>
+        <form onSubmit={addHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={usernameHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" value={age} onChange={ageHandler} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
