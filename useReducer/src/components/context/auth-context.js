9dd8d0324f716dd53add761cb45cb577458d7246
CraXWaR <craxwar@gmail.com>
2023-07-12 13:52:17 +0300

    Adding more context

--- useReducer/src/components/context/auth-context.js
+++ useReducer/src/components/context/auth-context.js
@@ -1,5 +1,43 @@
-import { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
-const AuthContext = createContext({ isLoggedIn: false });
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
