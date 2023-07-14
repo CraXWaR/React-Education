5f1e8b627258b5dc5bc28ec5a936bb7df5c445b0
CraXWaR <craxwar@gmail.com>
2023-07-14 11:32:36 +0300

    Filling the meals with the dummy meals

--- shoppingList/src/components/Meals/AvailableMeals.js
+++ shoppingList/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import classes from "./AvailableMeals.module.css";
+import Cart from "../UI/Cart";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -30,13 +32,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>
-    {meal.name}
-  </li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Cart>
+        <ul>{mealsList}</ul>
+      </Cart>
     </section>
   );
 };
