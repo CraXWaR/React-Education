fd68fa3a1acefba67cc85a637ef7e00db1c7b788
CraXWaR <craxwar@gmail.com>
2023-07-14 11:06:53 +0300

    Creating cart button

--- shoppingList/src/components/Layout/Header.js
+++ shoppingList/src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import React, { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderButton from "./HeaderButton";
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Shopping List</h1>
-        <button>Cart</button>
+        <HeaderButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Just image" />
