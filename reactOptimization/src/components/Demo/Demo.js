43d3a64f3b94a7dfcff272f00ec5ed6156b5f272
CraXWaR <craxwar@gmail.com>
2023-07-17 11:24:53 +0300

    .

--- reactOptimization/src/components/Demo/Demo.js
+++ reactOptimization/src/components/Demo/Demo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 const Demo = (props) => {
     return (
@@ -6,4 +6,4 @@ const Demo = (props) => {
     );
 }
 
-export default Demo
+export default memo(Demo);
