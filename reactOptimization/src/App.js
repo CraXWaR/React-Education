0dfecf36b6a492a6676a7315223728d863fea981
CraXWaR <craxwar@gmail.com>
2023-07-17 10:52:37 +0300

    .

--- reactOptimization/src/App.js
+++ reactOptimization/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [paragraph, setParagraph] = useState(false);
+
+  const howParagraph = () => {
+    setParagraph((prefParahraph) => ! prefParahraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {paragraph && <p>This is new paragraph!</p>}
+      <Button onClick={howParagraph}>Show new paragraph</Button>
     </div>
   );
 }
