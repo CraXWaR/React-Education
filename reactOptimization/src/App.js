3fa287e2bd7c68efbe696523c4391826292278e9
CraXWaR <craxwar@gmail.com>
2023-07-17 11:30:47 +0300

    Removing logs and formating

--- reactOptimization/src/App.js
+++ reactOptimization/src/App.js
@@ -1,14 +1,14 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo } from "react";
 
-import './App.css';
-import DemoList from './components/Demo/DemoList';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
+  const [listTitle, setListTitle] = useState("My List");
 
   const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
+    setListTitle("New Title");
   }, []);
 
   const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
--- reactOptimization/src/components/Demo/Demo.js
+++ reactOptimization/src/components/Demo/Demo.js
@@ -1,9 +1,7 @@
 import React, { memo } from "react";
 
 const Demo = (props) => {
-    return (
-        <p>{props.show ? 'This is new paragrah!' : ''}</p>
-    );
-}
+  return <p>{props.show ? "This is new paragrah!" : ""}</p>;
+};
 
 export default memo(Demo);
