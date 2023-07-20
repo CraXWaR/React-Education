c72b787e42777eb326446f74cdc08ac3d12c7113
CraXWaR <craxwar@gmail.com>
2023-07-20 11:56:49 +0300

    Finishing dumy project

--- classbasedComponents/src/components/User.js
+++ classbasedComponents/src/components/User.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import classes from './User.module.css';
+import React, { Component } from "react";
 
-class User extends Component { 
+import classes from "./User.module.css";
+
+class User extends Component {
+  componentWillUnmount() {
+    console.log("User will unmount!");
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
-  };
-};
+  }
+}
 
 // const User = (props) => {
 //   return <li className={classes.user}>{props.name}</li>;
--- classbasedComponents/src/components/Users.js
+++ classbasedComponents/src/components/Users.js
@@ -1,34 +1,44 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
+
 import User from "./User";
-
 import classes from "./Users.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
 
 class Users extends Component {
   constructor() {
     super();
     this.state = {
       showUsers: true,
+      more: "Test",
     };
   }
+
+  componentDidUpdate() {
+    // try {
+    //   someCodeWhichMightFail()
+    // } catch (err) {
+    //   // handle error
+    // }
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
   toggleUsersHandler() {
+    // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
     });
   }
+
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
     );
+
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
