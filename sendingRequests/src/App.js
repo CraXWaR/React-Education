9906c17d42228d3da4f4eaf277e0c47539e04918
CraXWaR <craxwar@gmail.com>
2023-07-21 12:09:21 +0300

    adding async/await

--- sendingRequests/src/App.js
+++ sendingRequests/src/App.js
@@ -6,22 +6,19 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMovies = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        const transformedMovied = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovied);
-      });
+  const fetchMovies = async () => {
+    const res = await fetch("https://swapi.dev/api/films/");
+    const data = await res.json();
+
+    const transformedMovied = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovied);
   };
 
   return (
