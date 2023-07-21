4f3b31cafda95dd494a98e5a1a56fb75275b3da1
CraXWaR <craxwar@gmail.com>
2023-07-21 12:04:36 +0300

    changing hardcodded dummy data to request from swapi

--- sendingRequests/src/App.js
+++ sendingRequests/src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const transformedMovied = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovied);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
