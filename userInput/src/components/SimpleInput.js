4eff9f3e731325420f6e89cb4c58ca2dd95a00ec
CraXWaR <craxwar@gmail.com>
2023-07-25 11:39:08 +0300

    getting user input and addind some vlaid

--- userInput/src/components/SimpleInput.js
+++ userInput/src/components/SimpleInput.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [enteredName, setEnteredName] = useState();
+  const [nameValid, setNameValid] = useState(true);
+
+  const nameChange = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setNameValid(false);
+      return;
+    }
+    setNameValid(true);
+
+    console.log(enteredName);
+    setEnteredName("");
+
+    const enteredNameRef = nameRef.current.value;
+    console.log(enteredNameRef);
+  };
+
+  const nameClasses = nameValid ? "form-control" : "form-control invalid";
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formHandler}>
+      <div className={nameClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          ref={nameRef}
+          onChange={nameChange}
+        />
+        {!nameValid && <p className="error-text">Name can't be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
