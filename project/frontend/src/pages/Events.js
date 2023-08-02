c0fe803002c4ff92458a8803d8ace005579fa41b
CraXWaR <craxwar@gmail.com>
2023-08-02 11:47:04 +0300

    Adding error page

--- project/frontend/src/pages/Events.js
+++ project/frontend/src/pages/Events.js
@@ -5,6 +5,11 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -16,7 +21,8 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //...
+    // return { isError: true, message: "Could not fetch events." };
+    throw { message: "Could not fetch events." };
   } else {
     return response;
   }
