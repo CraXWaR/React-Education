68e952ca4139668dae2f03e78fe9343e9b1d7ff9
CraXWaR <craxwar@gmail.com>
2023-08-02 13:09:20 +0300

    making error with less code

--- project/frontend/src/pages/Events.js
+++ project/frontend/src/pages/Events.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -21,10 +16,12 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events." };
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+    return json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
   } else {
     return response;
   }
