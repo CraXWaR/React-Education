dd60cd33f3125bdd02f9ef2dd8f4d7abe8f6940b
CraXWaR <craxwar@gmail.com>
2023-08-02 10:55:49 +0300

    Adding event details and outputing the id

--- project/frontend/src/pages/Events.js
+++ project/frontend/src/pages/Events.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "event 1",
+  },
+  {
+    id: "e2",
+    title: "event 2",
+  },
+];
 
 const EventsPage = () => {
-  return <h1>EventsPage</h1>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((e) => (
+          <li key={e.id}>
+            <Link to={e.id}>{e.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
