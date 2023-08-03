818d093cbe087b12f50459b0adf7651941b9bdde
CraXWaR <craxwar@gmail.com>
2023-08-03 13:58:13 +0300

    Finishing dummy project

--- userAuth/frontend/src/pages/Authentication.js
+++ userAuth/frontend/src/pages/Authentication.js
@@ -1,5 +1,6 @@
-import { json, redirect } from "react-router-dom";
-import AuthForm from "../components/AuthForm";
+import { json, redirect } from 'react-router-dom';
+
+import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
   return <AuthForm />;
@@ -7,40 +8,43 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-export const action = async ({ req }) => {
-  const params = new URL(req.url).searchParams;
-  const mode = params.get("mode") || "login";
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
 
-  if (mode !== "login" && mode !== "signup") {
-    throw json({ message: "Wrong Url" }, { status: 422 });
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
   }
 
-  const data = await req.formData();
+  const data = await request.formData();
   const authData = {
-    email: data.get("email"),
-    password: data.get("password"),
+    email: data.get('email'),
+    password: data.get('password'),
   };
 
-  const res = await fetch("http://localhost:8080/" + mode, {
-    method: "POST",
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(authData),
   });
 
-  if (res.status === 422 || res.status === 401) {
-    return req;
+  if (response.status === 422 || response.status === 401) {
+    return response;
   }
 
-  if (!res.ok) {
-    throw json({ message: "Could not auth user" }, { status: 500 });
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
   }
 
-  const restData = await res.json();
-  const token = restData.token;
+  const resData = await response.json();
+  const token = resData.token;
 
-  localStorage.setItem("token", token);
+  localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem('expiration', expiration.toISOString());
 
-  return redirect("/");
-};
+  return redirect('/');
+}
