4eba07f3e680e68626ec8101344f22c9e067d45e
CraXWaR <craxwar@gmail.com>
2023-08-08 11:45:41 +0300

    Linking between pages

--- nextJS/.next/static/chunks/webpack.js
+++ nextJS/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3b262890cce4460"; }
+/******/ 		__webpack_require__.h = function() { return "d48ee4ed600a31a7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
