84fe1d60435623eff0a99aa716368188934b8848
CraXWaR <craxwar@gmail.com>
2023-08-11 12:51:17 +0300

    Adding logs for events

--- animations/src/App.js
+++ animations/src/App.js
@@ -38,6 +38,12 @@ class App extends Component {
           timeout={300}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log("Enter")}
+          onEntering={() => console.log("Entering")}
+          onEntered={() => console.log("Entered")}
+          onExit={() => console.log("Exit")}
+          onExiting={() => console.log("Exiting")}
+          onExited={() => console.log("Exited")}
         >
           {(state) => (
             <div
